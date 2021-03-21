97af060b15dc3318771dc5e4a1df7b6d6ce06762
y-time <820019452@qq.com>
2021-03-22 00:33:16 +0800

    spring代码

--- spring/lagou-transfer/src/main/java/com/lagou/edu/utils/DruidUtils.java
+++ spring/lagou-transfer/src/main/java/com/lagou/edu/utils/DruidUtils.java
@@ -15,9 +15,9 @@ public class DruidUtils {
 
     static {
         druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        druidDataSource.setUrl("jdbc:mysql://localhost:3306/bank");
+        druidDataSource.setUrl("jdbc:mysql://localhost:3306/lagou");
         druidDataSource.setUsername("root");
-        druidDataSource.setPassword("123456");
+        druidDataSource.setPassword("root");
 
     }
 
